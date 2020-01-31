b3cb5600e2cad88326ea9f4f52301e6c6cc45c1f
chansol, Han <chansol0505@naver.com>
2020-02-01 00:36:55 +0900

    SUDO

--- setup.sh
+++ setup.sh
@@ -3,33 +3,38 @@
 FINAL_STAGE=12
 FIRST_PASSWORD="bof1"
 PASSWORDS_FILE="PASSWORDS"
+if [[ $UID == 0 ]]; then
+    SUDO=""
+else
+    SUDO="sudo "
+fi
 
 setting_ctf() {
     USER="bof$1"
     # set password
-    echo "$USER:$2" | sudo chpasswd
+    echo "$USER:$2" | $SUDO chpasswd
     if [[ $1 != $FINAL_STAGE ]]; then
         PWN="bof$(($1 + 1))"
         BIN="/home/$USER/$USER"
         SRC="/home/$USER/$USER.c"
-        sudo cp _$USER /home/$USER/$USER
-        sudo cp _$USER.c /home/$USER/$USER.c
-        sudo chown $PWN:$PWN $BIN
-        sudo chmod 6555 $BIN
-        sudo chown root:root $SRC
-        sudo chmod 644 $SRC
-        sudo cp ~/.gdbinit /home/$USER/
-        sudo sed -i 's@source ~@source '"$HOME"'@g' /home/$USER/.gdbinit
-        sudo chown $USER:$USER /home/$USER/.gdbinit
-        sudo cp ~/.tmux.conf /home/$USER/
-        sudo chown $USER:$USER /home/$USER/.tmux.conf
+        $SUDO cp _$USER /home/$USER/$USER
+        $SUDO cp _$USER.c /home/$USER/$USER.c
+        $SUDO chown $PWN:$PWN $BIN
+        $SUDO chmod 6555 $BIN
+        $SUDO chown root:root $SRC
+        $SUDO chmod 644 $SRC
+        $SUDO cp ~/.gdbinit /home/$USER/
+        $SUDO sed -i 's@source ~@source '"$HOME"'@g' /home/$USER/.gdbinit
+        $SUDO chown $USER:$USER /home/$USER/.gdbinit
+        $SUDO cp ~/.tmux.conf /home/$USER/
+        $SUDO chown $USER:$USER /home/$USER/.tmux.conf
         if [[ $1 == 11 ]]; then
             POC="exp_bof11.py"
             LIBC="libc.so.6"
-            sudo cp $POC /home/$USER
-            sudo chown $USER:$USER /home/$USER/$POC
-            sudo cp 2.23/lib/$LIBC /home/$USER
-            sudo chown $USER:$USER /home/$USER/$LIBC
+            $SUDO cp $POC /home/$USER
+            $SUDO chown $USER:$USER /home/$USER/$POC
+            $SUDO cp 2.23/lib/$LIBC /home/$USER
+            $SUDO chown $USER:$USER /home/$USER/$LIBC
         fi
     else
         echo "R=\"\e[1;31m\"
@@ -41,7 +46,7 @@ function pause(){
     read -p \"\$*\"
 }
 pause 'Press [Enter] to exit...'
-exit" | sudo tee /home/$USER/.bashrc
+exit" | $SUDO tee /home/$USER/.bashrc
     fi
 }
 
@@ -49,8 +54,8 @@ cleanup(){
     make clean
     rm $PASSWORDS_FILE
     for ((i=1; i<=$FINAL_STAGE; i++)); do
-        sudo userdel bof$i
-        sudo rm -rf /home/bof$i
+        $SUDO userdel bof$i
+        $SUDO rm -rf /home/bof$i
         rm -rf _bof$i.c
     done
 }
@@ -59,12 +64,12 @@ compile() {
     distro=$(cat /etc/os-release | grep "^ID=" | cut -d\= -f2 | sed -e 's/"//g')
     case "$distro" in
     "ubuntu")
-        sudo apt-get -y install gcc-multilib
+        $SUDO apt-get -y install gcc-multilib
         ;;
     "arch")
         LIB32="lib32-gcc-libs"
         if ! pacman -Qi $LIB32>/dev/null; then
-            sudo pacman -S --noconfirm $LIB32
+            $SUDO pacman -S --noconfirm $LIB32
         fi
         ;;
     esac
@@ -85,10 +90,10 @@ setup_ctf() {
             PASSWORD=$(cat /dev/urandom | hexdump -n 2 -e '"%02x"')
             PW_FILE="bof$i.pw"
             echo $PASSWORD > $PW_FILE
-            sudo mv $PW_FILE /home/bof$((i - 1))
+            $SUDO mv $PW_FILE /home/bof$((i - 1))
             FLAG=/home/bof$((i - 1))/$PW_FILE
-            sudo chown root:bof$i $FLAG
-            sudo chmod 440 $FLAG
+            $SUDO chown root:bof$i $FLAG
+            $SUDO chmod 440 $FLAG
         fi
         echo -e "bof$i\t$PASSWORD" >> $PASSWORDS_FILE
         # setting CTFs(adding user, copying files, setting authority)
@@ -106,7 +111,7 @@ main(){
         # Create users
         #
         for ((i=1; i<=$FINAL_STAGE; i++)); do
-            sudo useradd -m -d /home/bof$i -s $(which bash) bof$i
+            $SUDO useradd -m -d /home/bof$i -s $(which bash) bof$i
             # Assign uid of created user to the source code
             if [[ $i != "1" ]]; then
                 _UID=$(cat /etc/passwd | grep "bof$i:" | cut -d ':' -f3)
